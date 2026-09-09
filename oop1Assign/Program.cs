fdf9a5e038bc8a8bbacb585e590001cd33c7c363
yousif <yshehab693@gmail.com>
2026-09-09 22:45:18 +0300

    Question 1 b)

--- oop1Assign/Program.cs
+++ oop1Assign/Program.cs
@@ -8,7 +8,9 @@
             // The modifiaction will be done in the copy only and the original one will not be modified because struct is value type 
             #endregion
 
-            
+            #region Q 1 b)What happens when a Customer variable is copied into another variable and one variable modifies the object
+            // The two variables will have the same modification because class is refrene type 
+            #endregion
         }
     }
 }
