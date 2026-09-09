c3f70cdf109b4c8744c3a1cb884999ce484fc1e7
yousif <yshehab693@gmail.com>
2026-09-09 22:41:32 +0300

    Question 1 a)

--- oop1Assign/Program.cs
+++ oop1Assign/Program.cs
@@ -7,6 +7,8 @@
             #region Q 1 a)	What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
             // The modifiaction will be done in the copy only and the original one will not be modified because struct is value type 
             #endregion
+
+            
         }
     }
 }
