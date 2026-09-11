4cb4de233c840f8c65574fe030be014dfb0a5543
yousif <yshehab693@gmail.com>
2026-09-11 16:57:38 +0300

    Part 2 Q2 Shipment Struct

---
+++ oop1Assign/Shipment.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop1Assign
+{
+    internal struct Shipment
+    {
+        private string trackingCode;
+        private string description;
+        private double weight;
+        private double deliveryFee;
+
+        public string Destination { get; set; }
+        public string TrackingCode
+        {
+            get
+            {
+                return trackingCode;
+            }
+            private set
+            {
+              if(!string.IsNullOrEmpty(value))
+              {
+                  trackingCode = value;
+              }
+            }
+
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    description = value;
+                }
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+            }
+        }
+        public double DeliveryFee
+        {
+            get
+            {
+                return deliveryFee;
+            }
+            private set
+            {
+                if(value > 0)
+                {
+                    deliveryFee = value;
+                }
+            }
+        }
+        public double EstimatedCost {
+            get
+            {
+                return deliveryFee + (weight * 5);
+            }
+
+        }
+    }
+}
