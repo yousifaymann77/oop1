6b92c0835afd3834e5d4948138cf565e1b2cdaee
yousif <yshehab693@gmail.com>
2026-09-11 22:26:18 +0300

    part 2 Q5 DeliveryCenter struct

---
+++ oop1Assign/DeliveryCenter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop1Assign
+{
+    internal struct DeliveryCenter
+    {
+        private Shipment[] shipments ;
+
+        public DeliveryCenter()
+        {
+            shipments = new Shipment[10];
+        }
+
+        public Shipment this[int index]
+        {
+            get 
+            {
+                if (index >= 0 && index < shipments.Length)
+                {
+                    return shipments[index];
+                }
+                else return default;
+            }
+            set { 
+              if (index >= 0 && index < shipments.Length)
+                shipments[index] = value;
+            }
+        }
+        public Shipment this[string trackingCode]
+        {
+            get {
+                for (int i = 0; i < shipments.Length; i++ )
+                {
+                    if (shipments[i].TrackingCode == trackingCode)
+                    {
+                        return shipments[i];
+                    }
+                }
+                return default;
+            }
+        }
+
+        public bool AddShipment(Shipment shipment)
+        {
+            for (int i = 0; i < shipments.Length; i++)
+            {
+                if (shipments[i].TrackingCode == null)
+                {
+                    shipments[i] = shipment;
+                    return true;
+                    
+                }
+            }
+            return false;
+        }
+    }
+}
